4f786b416e17f027fd83ac28cc3cc1ced0534112
Lucas Bereta <lucas.bereta@hotmail.com>
2024-09-03 20:37:56 -0300

    :bug: Fix correct credentials DB

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/ecommerce_db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://admin:admin@localhost:5432/ecommerce_db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
